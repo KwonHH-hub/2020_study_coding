1be35a1d46299ebf14db881efa89747a4b4829dd
KwonHH-hub <zkzh6hn9@naver.com>
2020-09-02 12:18:38 +0900

    commit

--- baekjoon/baekjoon2839.py
+++ baekjoon/baekjoon2839.py
@@ -26,7 +26,7 @@
 # weight = int(input())
 # if weight % 5 == 0:
 #     print(weight // 5)
-# else :
+# else:
 #     if (weight % 5) % 3 == 0:
 #         print(weight//5 + (weight - weight//5 * 5) // 3)
 #     elif weight % 3 == 0:
@@ -34,3 +34,43 @@
 #     else :
 #         print(-1)
 
+# 틀린코드
+# weight = int(input())
+# count = 0
+# while 1:
+#     if weight % 5 == 0:
+#         print(count + weight // 5)
+#         break
+#     elif weight % 3 == 0:
+#         print(count + weight // 3)
+#         break
+#     else:
+#         weight -= 5
+#         if weight < 0:
+#             print(-1)
+#             break
+#         count += 1
+#         continue
+
+# weight = int(input())
+# count = 0
+# while 1:
+#     if weight > 5:
+#         weight -= 5
+#         count += 1
+#         if weight % 5 == 0:
+#             print(count + (weight // 5))
+#             break
+#         elif weight % 3 == 0:
+#             print(count + (weight // 3))
+#             break
+#     else:
+#         if weight % 5 == 0:
+#             print(count + (weight // 5))
+#             break
+#         elif weight % 3 == 0:
+#             print(count + (weight // 3))
+#             break
+#         else:
+#             print(-1)
+#             break
