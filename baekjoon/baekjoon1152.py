39db0ca83271dbf2d74993f11e6d26cd216a9c86
KwonHH-hub <khh202151@gmail.com>
2020-12-16 00:48:12 +0900

    commit(실패)

---
+++ baekjoon/baekjoon1152.py
@@ -0,0 +1,11 @@
+import sys
+
+# st = sys.stdin.readline().strip().split(' ')
+# st = 'Merry-christmas haha haha'
+#
+# st = st.strip().split(' ')
+# print(st)
+
+st = input().strip().split(' ')
+print(len(st))
+
