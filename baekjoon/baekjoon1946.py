914fd67183915301fcdceb1af5909d5cef7f5368
KwonHH-hub <khh202151@gmail.com>
2020-12-17 20:01:37 +0900

    commit(실패)

--- baekjoon/baekjoon1946.py
+++ baekjoon/baekjoon1946.py
@@ -1,89 +1,29 @@
-# import sys
-# import time
-#
-# def employ (tempB):
-#     count = 1
-#     for tb in range(1, len(tempB)):
-#         ref = tempB[0:tb]
-#         ref.sort()
-#         if ref[0] > tempB[tb]:
-#             count += 1
-#     return count
-#
-#
-#
-# case = int(sys.stdin.readline())
-#
-#
-# while case > 0:
-#
-#     tempA = []
-#     tempB = []
-#     A = []
-#     B = []
-#
-#     time1 = time.time()
-#
-#     people = int(sys.stdin.readline())
-#
-#     for _ in range(people):
-#         a, b = sys.stdin.readline().split(' ')
-#         A.append(int(a))
-#         B.append(int(b))
-#
-#     tempA = A[:]
-#     tempA.sort()
-#     tempB = []
-#     for ta in range(len(tempA)):
-#         i = A.index(tempA[ta])
-#         tempB.append(B[i])
-#
-#     print(employ(tempB))
-#     tempB = []
-#     case -= 1
-#
-#     time2 = time.time()
-#     print('time : ',case, '  ',time2-time1)
+def count(rank):
+    cnt = 0
+    for a in range(len(rank)):
+        if a == 0:
+            min = rank[a][1]
+            cnt += 1
+            continue
+        if rank[a][1] < min:
+            cnt += 1
+            min = rank[a][1]
+    return cnt
 
 
-def re_arr(A,B):
-    tmpA = A[:]
-    tmpB = []
 
-    tmpA.sort()
-    for a in range(len(A)):
-        idx = A.index(tmpA[a])
-        tmpB.append(B[idx])
-    return tmpB
-
-def find(A, B):
-    for i in range(len(A)):
-        ref = B[i]
-        tmp = A[0:i]
-        tmp.sort()
 
 
 case = int(input())
-print(case)
 while case > 0:
-    A = []
-    B = []
+    rank = []
+
     people = int(input())
     while people > 0:
         a, b = map(int, input().split(' '))
-        A.append(a)
-        B.append(b)
+        rank.append((a,b))
         people -= 1
-
-    print(A)
-    print(B)
-
-    tmpA = A[:]
-    tmpA.sort()
-    tmpB = re_arr(A,B)
-
-    print('tmpA ', tmpA)
-    print('tmpB ', tmpB)
+    rank_ = sorted(rank)
+    print(count(rank_))
 
     case -= 1
-
